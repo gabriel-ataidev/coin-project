9fbb600e4a585ff07fdf94c96c292bc3c47ff667
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-08-09 10:04:17 -0300

    setting up projects component

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,25 @@
 import Message from "../layout/Message";
-import {useLocation} from 'react-router-dom'
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+import {useLocation} from "react-router-dom";
+import styles from "./Projects.module.css";
 
 function Projects() {
-    const location = useLocation()
-    let message = ''
-    if(location.state) {
-        message = location.state.message
-    }
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto"/>
+      </div>
       {message && <Message msg={message} type="success" />}
+      <Container customClass="start">
+        <p>projetos...</p>
+      </Container>
     </div>
   );
 }

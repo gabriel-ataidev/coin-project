c9e848969ac50bb5566c22f0923e6d410dfef647
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-08-09 11:52:59 -0300

    loader component all set

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,7 @@
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
+import Loading from "../layout/Loading";
 import {useLocation} from "react-router-dom";
 import styles from "./Projects.module.css";
 import ProjectCard from "../project/ProjectCard";
@@ -8,7 +9,7 @@ import {useState, useEffect} from "react";
 
 function Projects() {
   const [projects, setProjects] = useState([]);
-
+  const [removeLoading, setRemoveLoading] = useState(false)
   const location = useLocation();
   let message = "";
   if (location.state) {
@@ -16,17 +17,24 @@ function Projects() {
   }
 
   useEffect(() => {
-      fetch("http://localhost:5000/projects", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((resp) => resp.json())
-        .then((data) => {
-          setProjects(data);
-        });
-  }, []);
+    // Para ver o loading
+    setTimeout(
+      () =>
+        fetch('http://localhost:5000/projects', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setProjects(data)
+            setRemoveLoading(true)
+          }),
+      500,
+    )
+  }, [])
+
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
@@ -45,6 +53,10 @@ function Projects() {
               key={project.id}
             />
           ))}
+          {!removeLoading && <Loading/>}
+          {removeLoading && projects.length === 0 && (
+          <p>Não há projetos cadastrados!</p>
+        )}
       </Container>
     </div>
   );

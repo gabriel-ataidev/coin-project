41f734b8355f2e7f3d2f19e962c055065de711d7
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-08-09 11:33:08 -0300

    project card done

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -3,7 +3,8 @@ function Select({options, text, name, handleOnChange, value}) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      <select name={name} id={name} onChange={handleOnChange}>
+      <select  value={value || ''} required name={name} id={name} onChange={handleOnChange}>
+      <option>Selecione uma opção</option>
         {options.map(
           (option) => (
             <option value={option.id} key={option.id}>{option.name}</option>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,22 +3,48 @@ import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
 import {useLocation} from "react-router-dom";
 import styles from "./Projects.module.css";
+import ProjectCard from "../project/ProjectCard";
+import {useState, useEffect} from "react";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+      fetch("http://localhost:5000/projects", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProjects(data);
+        });
+  }, []);
   return (
     <div className={styles.project_container}>
       <div className={styles.title_container}>
         <h1>Meus Projetos</h1>
-        <LinkButton to="/newproject" text="Criar Projeto"/>
+        <LinkButton to="/newproject" text="Criar Projeto" />
       </div>
       {message && <Message msg={message} type="success" />}
       <Container customClass="start">
-        <p>projetos...</p>
+      {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category ? project.category.name : project.category}
+              key={project.id}
+            />
+          ))}
       </Container>
     </div>
   );

1cab6b9de29cb94c05f1252d8666d744283a668d
Gabriel Ataide <86975073+gabriel-ataidev@users.noreply.github.com>
2022-08-09 14:09:08 -0300

    showing project page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Container from "./components/layout/Container";
@@ -19,6 +20,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer/>
